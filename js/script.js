618d64fc634528653960392fa094bcf0705062cd
Marek Skolik <marek250296@interia.pl>
2022-08-13 17:44:41 +0200

    clickedElement

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@
   }); */
 
 const titleClickHandler = function (event) {
+  const clickedElement = this;
+  console.log('clickedElement (with plus): ' + clickedElement);
   console.log('Link was clicked!');
   console.log(event);
 
 
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -17,9 +19,12 @@ const titleClickHandler = function (event) {
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  clickedElement.classList.add('active');
+  console.log(clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.post');
 

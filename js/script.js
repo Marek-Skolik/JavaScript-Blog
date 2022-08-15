8614a03f7c3c053577ddb827d29e040195653ad3
Marek Skolik <marek250296@interia.pl>
2022-08-15 22:30:39 +0200

    handler

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@
   }); */
 
 const titleClickHandler = function (event) {
+  event.preventDefault();
   const clickedElement = this;
-  console.log('clickedElement (with plus): ' + clickedElement);
+
   console.log('Link was clicked!');
   console.log(event);
-
 
 
   /* [DONE] remove class 'active' from all article links  */
@@ -19,7 +19,7 @@ const titleClickHandler = function (event) {
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   clickedElement.classList.add('active');
   console.log(clickedElement);
@@ -34,9 +34,15 @@ const titleClickHandler = function (event) {
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
